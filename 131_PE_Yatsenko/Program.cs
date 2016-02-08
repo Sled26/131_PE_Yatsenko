684c36f1553121960d033c167e7a644b10898b8e
Sled26 <Sledopyt26@mail.ru>
2016-02-08 12:24:35 +0300

    Лабораторная 1, задание 1. Расчеты дневной оплаты сотрудника

---
+++ 131_PE_Yatsenko/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _131_PE_Yatsenko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Double b;
+            Console.Write("Выручка за сегодня:");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a <= 10000)
+                Console.WriteLine(b = a * 0.01 * 5);
+            else
+                Console.WriteLine(b = 10000 * 0.01 * 5 + (a - 10000) * 0.01 * 10);
+            Console.ReadLine();
+        }
+    }
+}
